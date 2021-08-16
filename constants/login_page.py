9b729f01acb39e85f4579d1a4b4d5bfe1f414def
Helen <lena.mzurets@gmail.com>
2021-08-16 17:32:29 +0300

    Constants without helpers

--- constants/login_page.py
+++ constants/login_page.py
@@ -1,31 +1,29 @@
 class LoginPageConstants:
     """Store constants related to Login Page"""
 
-    # Sign Up
-    SIGN_IN_USERNAME_XPATH=".//input[@id='username-register']"
+    # Sign in
+    SIGN_IN_USERNAME_XPATH=".//input[@placeholder='Username']"
+    SIGN_IN_PASSWORD_XPATH=".//input[@placeholder='Password']"
+    SIGN_IN_BUTTON_XPATH=".//button[contains(text(), 'Sign In')]"
+
     SIGN_UP_USERNAME_XPATH='.//*[@id="username-register"]'
     SIGN_UP_EMAIL_XPATH='.//*[@id="email-register"]'
     SIGN_UP_PASSWORD_XPATH='.//*[@id="password-register"]'
     SIGN_UP_BUTTON_XPATH='.//*[@id="registration-form"]/button'
 
-    # Sign in
-    SIGN_IN_SURNAME_XPATH=".//input[@placeholder='Username']"
-    SIGN_IN_PASSWORD_XPATH=".//input[@placeholder='Password']"
-    SIGN_IN_BUTTON_TEXT="Sign In"
+    # Messages
+    SIGN_IN_EMPTY_FIELDS_LOGIN_XPATH=".//div[contains(text(), 'Invalid username / password')]"
+    ERROR_MESSAGE_CYRILLIC_USER_NAME_XPATH='//*[@id="registration-form"]/div[1]/div'
+    ERROR_MESSAGE_EMPTY_USERNAME_FIELDS_XPATH='.//*[@id="registration-form"]/div[1]/div'
+    ERROR_MESSAGE_EMAIL_FIELDS_XPATH='.//*[@id="registration-form"]/div[2]/div'
+    ERROR_MESSAGE_PASSWORD_EMPTY_FIELDS_XPATH='.//*[@id="registration-form"]/div[3]/div'
 
-    # Message
-    INVALID_LOGIN_MESSAGE_TEXT='Invalid username / password'
-    EMPTY_USERNAME_IN_REGISTRATION_FORM_TEXT="Username must be at least 3 characters."
-    EMPTY_USERNAME_IN_REGISTRATION_FORM_XPATH='.//*[@id="registration-form"]/div[1]/div'
-    EMPTY_EMAIL_IN_REGISTRATION_FORM_TEXT='You must provide a valid email address.'
-    EMPTY_EMAIL_IN_REGISTRATION_FORM_XPATH='.//*[@id="registration-form"]/div[2]/div'
-    EMPTY_PASSWORD_IN_REGISTRATION_FORM_TEXT='Password must be at least 12 characters.'
-    EMPTY_PASSWORD_IN_REGISTRATION_FORM_XPATH='.//*[@id="registration-form"]/div[3]/div'
-    USER_ALREADY_TAKEN_TEXT='That username is already taken.'
-    USER_ALREADY_TAKEN_XPATH='.//*[@id="registration-form"]/div[1]/div'
-    EMAIL_ALREADY_USED_TEXT='That email is already being used.'
-    EMAIL_ALREADY_USED_XPATH='.//*[@id="registration-form"]/div[2]/div'
-    PASSWORD_MORE_CHARACTERS_TEXT='Password cannot exceed 50 characters'
-    PASSWORD_MORE_CHARACTERS_XPATH='.//*[@id="registration-form"]/div[3]/div'
-    CYRRILIC_VALUE_MESSAGE_TEXT='Username can only contain letters and numbers.'
-    CYRRILIC_VALUE_MESSAGE_XPATH='//*[@id="registration-form"]/div[1]/div'
+    # Texts
+    SIGN_IN_EMPTY_FIELDS_LOGIN_TEXT='Invalid username / password'
+    ERROR_MESSAGE_CYRILLIC_USER_NAME_TEXT='Username can only contain letters and numbers.'
+    ERROR_MESSAGE_EMPTY_USERNAME_FIELDS_TEXT='Username must be at least 3 characters.'
+    ERROR_MESSAGE_EMAIL_FIELDS_TEXT='You must provide a valid email address.'
+    ERROR_MASSAGE_EMAIL_ALREADY_USED_TEXT='That email is already being used.'
+    ERROR_MESSAGE_PASSWORD_EMPTY_FIELDS_TEXT='Password must be at least 12 characters.'
+    ERROR_MESSAGE_USER_ALREADY_TAKEN_TEXT='That username is already taken.'
+    ERROR_MESSAGE_PASSWORD_CANNOT_EXCEED_TEXT='Password cannot exceed 50 characters'
