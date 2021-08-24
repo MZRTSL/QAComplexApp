98a7bd14a563c7926811301ba84d80ee7b227582
Helen <lena.mzurets@gmail.com>
2021-08-24 16:41:54 +0300

    Constants without helpers

---
+++ constants/base.py
@@ -0,0 +1,3 @@
+class BaseConstants:
+    DRIVER_PATH=r'C:\Users\Helen\PycharmProjects\QAComplexApp\drivers\chromedriver'
+    START_PAGE_URL='https://qa-complex-app-for-testing.herokuapp.com/'
---
+++ constants/login_page.py
@@ -0,0 +1,31 @@
+class LoginPageConstants:
+    """Store constants related to Login Page"""
+
+    # Sign in
+    SIGN_IN_USERNAME_XPATH=".//input[@placeholder='Username']"
+    SIGN_IN_PASSWORD_XPATH=".//input[@placeholder='Password']"
+    SIGN_IN_BUTTON_XPATH=".//button[contains(text(), 'Sign In')]"
+
+    SIGN_UP_USERNAME_XPATH='.//*[@id="username-register"]'
+    SIGN_UP_EMAIL_XPATH='.//*[@id="email-register"]'
+    SIGN_UP_PASSWORD_XPATH='.//*[@id="password-register"]'
+    SIGN_UP_BUTTON_XPATH='.//*[@id="registration-form"]/button'
+
+    # Messages
+    HELLO_MESSAGE_XPATH='/html/body/div[2]/div[1]'
+    SIGN_IN_EMPTY_FIELDS_LOGIN_XPATH=".//div[contains(text(), 'Invalid username / password')]"
+    ERROR_MESSAGE_CYRILLIC_USER_NAME_XPATH='//*[@id="registration-form"]/div[1]/div'
+    ERROR_MESSAGE_EMPTY_USERNAME_FIELDS_XPATH='.//*[@id="registration-form"]/div[1]/div'
+    ERROR_MESSAGE_EMAIL_FIELDS_XPATH='.//*[@id="registration-form"]/div[2]/div'
+    ERROR_MESSAGE_PASSWORD_EMPTY_FIELDS_XPATH='.//*[@id="registration-form"]/div[3]/div'
+
+    # Texts
+    HELLO_MESSAGE_TEXT="Invalid username / password"
+    SIGN_IN_EMPTY_FIELDS_LOGIN_TEXT='Invalid username / password'
+    ERROR_MESSAGE_CYRILLIC_USER_NAME_TEXT='Username can only contain letters and numbers.'
+    ERROR_MESSAGE_EMPTY_USERNAME_FIELDS_TEXT='Username must be at least 3 characters.'
+    ERROR_MESSAGE_EMAIL_FIELDS_TEXT='You must provide a valid email address.'
+    ERROR_MASSAGE_EMAIL_ALREADY_USED_TEXT='That email is already being used.'
+    ERROR_MESSAGE_PASSWORD_EMPTY_FIELDS_TEXT='Password must be at least 12 characters.'
+    ERROR_MESSAGE_USER_ALREADY_TAKEN_TEXT='That username is already taken.'
+    ERROR_MESSAGE_PASSWORD_CANNOT_EXCEED_TEXT='Password cannot exceed 50 characters'
